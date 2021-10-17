eabcfea3e182d9c55fbf17cfde1b917d352e9cfc
Ibrahim Al-Alali <alali@live.de>
2021-10-17 02:46:24 +0200

    Redo

--- Foursouls Cards Initializer/Foursouls Cards Initializer/CardModel.cs
+++ Foursouls Cards Initializer/Foursouls Cards Initializer/CardModel.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Foursouls_Cards_Initializer
 {
-    class CardModel
+    public class CardModel
     {
-        public string Name { set; get; }
-        public string Expansion { set; get; }
         public int Id { set; get; }
+        public string Name { set; get; }
+        public string CardPack { set; get; }
         public string Type { set; get; }
 
         public CardModel(string name, string expension, string type, int id)
         {
             Name = name;
-            Expansion = expension;
+            CardPack = expension;
             Type = type;
             Id = id;
         }
 
         public string ToLine()
         {
-            return string.Format("{0}\t{1}\t{2}", Id, Name, Expansion) + Environment.NewLine;
+            return string.Format("{0}\t{1}\t{2}", Id, Name, CardPack) + Environment.NewLine;
         }
     }
 }
--- Foursouls Cards Initializer/Foursouls Cards Initializer/MainWindow.xaml.cs
+++ Foursouls Cards Initializer/Foursouls Cards Initializer/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using Windows.Storage.Pickers;
 using System.Threading.Tasks;
 using Microsoft.UI.Dispatching;
 using Windows.Storage;
+using Windows.ApplicationModel.DataTransfer;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,13 +29,14 @@ namespace Foursouls_Cards_Initializer
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private IntPtr m_hwnd;
-        private string message = "%s out of %s done (%s%%)";
+        private readonly IntPtr m_hwnd;
+        private readonly string message = "%s out of %s done (%s%%)";
+        HashSet<string> folders = new HashSet<string>();
 
         private Dictionary<string, int> IdOffsets = new()
         {
             { "Characters", 10000 },
-            { "Eternals", 20000 },
+            { "Starting Items", 20000 },
             { "Monsters", 30000 },
             { "Treasure", 40000 },
             { "Loot", 50000 },
@@ -57,14 +59,12 @@ namespace Foursouls_Cards_Initializer
 
         private async void OpenFolderDialog()
         {
-            ProgressBarIndicator.IsIndeterminate = true;
-
             FolderPicker folderPicker = new FolderPicker();
             folderPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
             folderPicker.FileTypeFilter.Add("*");
+            folderPicker.ViewMode = PickerViewMode.List;
 
             WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, m_hwnd);
-            DispatcherQueue dispatcherQueue = DispatcherQueue.GetForCurrentThread();
 
             StorageFolder folder = await folderPicker.PickSingleFolderAsync();
 
@@ -74,152 +74,74 @@ namespace Foursouls_Cards_Initializer
                 // (including other sub-folder contents)
                 Windows.Storage.AccessCache.StorageApplicationPermissions.
                 FutureAccessList.AddOrReplace("PickedFolderToken", folder);
-                FolderPath.Text = "Picked folder: " + folder.Path;
+                addToUi(folder.Name);
+            }
+        }
 
+        private async void Folders_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var storageItems = await e.DataView.GetStorageItemsAsync();
 
-                await DoWork(dispatcherQueue, folder.Path);
+                foreach (var item in storageItems)
+                {
+                    if (item.Attributes.HasFlag(Windows.Storage.FileAttributes.Directory))
+                    {
+                        addToUi(item.Path);
+                    }
+                }
+            }
+
+            
+
+            //dragAndDropImage.Visibility = Visibility.Collapsed;
+            //e.Handled = true;
+        }
+
+        private async void Folders_DragOver(object sender, DragEventArgs e)
+        {
+
+            bool areDirectories = true;
+
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var storageItems = await e.DataView.GetStorageItemsAsync();
+
+                foreach (var item in storageItems)
+                {
+                    if (!item.Attributes.HasFlag(Windows.Storage.FileAttributes.Directory))
+                    {
+                        areDirectories = false;
+                    }
+                }
+            }
+
+            if (areDirectories)
+            {
+                //dragAndDropImage.Visibility = Visibility.Visible;
+                e.AcceptedOperation = DataPackageOperation.Copy;
             }
             else
             {
-                ProgressBarIndicator.IsIndeterminate = false;
+                e.AcceptedOperation = DataPackageOperation.None;
             }
         }
 
-        private async Task DoWork(DispatcherQueue dispatcherQueue, string path)
+        private void Folders_DragLeave(object sender, DragEventArgs e)
         {
-            await Task.Run(() => InitialProcess(dispatcherQueue, path));
+            //dragAndDropImage.Visibility = Visibility.Collapsed;
         }
 
-        private async void InitialProcess(DispatcherQueue dispatcherQueue, string path)
+        private void addToUi(string folderPath)
         {
-            var files = Directory.GetFiles(path);
-            bool found = false;
+            folders.Add(folderPath);
+            Folders.Text = "";
 
-            foreach (var item in files)
+            foreach (var item in folders)
             {
-                if (item.EndsWith("process"))
-                {
-                    found = true;
-                    break;
-                }
+                Folders.Text += Path.GetFileName(item) + Environment.NewLine;
             }
-
-            if (!found)
-            {
-                dispatcherQueue.TryEnqueue(() =>
-                {
-                    ProgressBarIndicator.ShowError = true;
-                    ProgressText.Text = "Not the correct folder. Couldn't find the file \"process\"";
-                });
-
-                return;
-            }
-
-            int maxToProcess = GetNumberOfFiles(path) - 1;
-
-            dispatcherQueue.TryEnqueue(() =>
-            {
-                ProgressBarIndicator.Maximum = maxToProcess;
-                ProgressText.Text = string.Format(message, 0, maxToProcess, 0);
-            });
-
-            List<CardModel> cards = GetAllCards(path);
-            Dictionary<string, string> fileContent = new Dictionary<string, string>();
-
-            for (int i = 0; i < cards.Count; i++)
-            {
-                CardModel item = cards[i];
-                dispatcherQueue.TryEnqueue(() =>
-                {
-                    ProgressText.Text = string.Format(message, (i + 1), cards.Count, (((i + 1) / cards.Count) * 100));
-                });
-
-                if (fileContent.ContainsKey(item.Type))
-                {
-                    fileContent[item.Type] += item.ToLine();
-                }
-                else
-                {
-                    fileContent.Add(item.Type, item.ToLine());
-                }
-            }
-
-            Directory.CreateDirectory(Path.Combine(path, "result"));
-       
-            foreach (var item in fileContent)
-            {
-                var stream = File.Create(Path.Combine(path, "result", item.Key + ".txt"));
-                stream.Dispose();
-
-                File.AppendAllText(Path.Combine(path, "result", item.Key+ ".txt"), item.Value);
-            }
-
-            // todo the background process doesn't stop
-            // the iu does not update at all
-        }
-
-        private int GetNumberOfFiles(string folderName)
-        {
-            var folder = Directory.EnumerateDirectories(folderName);
-            int count = 0;
-
-            foreach (var item in folder)
-            {
-               count += GetNumberOfFiles(item);
-            }
-
-            return count + Directory.GetFiles(folderName).Length;
-        }
-
-        private List<CardModel> GetAllCards(string folderName)
-        {
-            var folder = Directory.EnumerateDirectories(folderName);
-            var list = new List<CardModel>();
-
-            foreach (var item in folder)
-            {
-                list.AddRange(GoToType(item));
-            }
-
-            return list;
-        }
-
-        private List<CardModel> GoToType(string expansion)
-        {
-            var list = new List<CardModel>();
-            var folder = Directory.EnumerateDirectories(expansion);
-
-            foreach (var item in folder)
-            {
-                list.AddRange(GoToFile(expansion, item));
-            }
-
-            return list;
-        }
-
-        private List<CardModel> GoToFile(string expansion, string type)
-        {
-            var list = new List<CardModel>();
-            var files = Directory.GetFiles(type);
-
-            List<string> names = new List<string>(files)
-                .Maped((x) => { return Path.GetFileName(x); })
-                .Sorted((x, y) => string.Compare(x, y, StringComparison.OrdinalIgnoreCase));
-
-            var id = IdOffsets[Path.GetFileName(type)];
-
-            for (int i = 0; i < names.Count; i++)
-            {
-                string item = names[i];
-                list.Add(new CardModel(
-                    item,
-                    Path.GetFileName(expansion),
-                    Path.GetFileName(type),
-                    id + i + 1
-               ));
-            }
-
-            return list;
         }
     }
 }
